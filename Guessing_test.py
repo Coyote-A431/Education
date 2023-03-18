64c8483f93980e01cb2efb39ada258adbfca56f6
Unknown <as1qwe12@gmail.com>
2023-03-18 14:58:17 +0400

    Final for first 4 tasks

--- Guessing_test.py
+++ Guessing_test.py
@@ -1,12 +1,17 @@
 import random
 import time
 import os
-import traceback
 
 class Guess_Test:
 
     def __init__(self):
         self.filepath = self.create_logs()
+        global previous_input
+        global is_first_iteration
+        global is_user_guess
+        previous_input = -1
+        is_first_iteration = True
+        is_user_guess = False
 
     def random_number(self):
         random_number = random.randint(1, 100)
@@ -14,31 +19,53 @@ class Guess_Test:
         return random_number
 
     def validation(self):
-        is_valid_input = False
-        while not is_valid_input:
-            try:
-                user_input = input('Guess the number: ')
-                with open(self.filepath, 'a+') as add_user_values:
-                    add_user_values.write(user_input + '\n')
-                current_input = int(user_input)
-                is_valid_input = True
-            except ValueError:
-                print('You entered not number or left empty field')
-        return current_input
+        user_input = input('Guess the number from 1 to 100: ')
+        with open(self.filepath, 'a+') as add_user_values:
+            add_user_values.write(f'[{user_input}]' + '\n')
+        return user_input
 
     def comparison(self, current_input, number_to_guess):
-        if current_input == number_to_guess:
-            print('You are great!')
-        elif current_input in range(number_to_guess - 10, number_to_guess + 11):
-            print('Very close')
-        elif current_input in range(number_to_guess - 25, number_to_guess + 26) \
-                and current_input not in range(number_to_guess - 10, number_to_guess + 11):
-            print('Close')
-        elif current_input in range(number_to_guess - 50, number_to_guess + 51) \
-                and current_input not in range(number_to_guess - 25, number_to_guess + 26):
-            print('Far')
-        else:
-            print('Very far')
+        global previous_input
+        global is_first_iteration
+        global is_user_guess
+        try:
+            if current_input == number_to_guess:
+                print('You are great!')
+                is_user_guess = True
+            elif current_input in range(number_to_guess - 10, number_to_guess + 11):
+                print('Very close')
+            elif current_input in range(number_to_guess - 25, number_to_guess + 26) \
+                    and current_input not in range(number_to_guess - 10, number_to_guess + 11):
+                print('Close')
+            elif current_input in range(number_to_guess - 50, number_to_guess + 51) \
+                    and current_input not in range(number_to_guess - 25, number_to_guess + 26):
+                print('Far')
+            else:
+                print('Very far')
+            if not is_first_iteration:
+                if previous_input != current_input:
+                    if previous_input < number_to_guess and not is_user_guess:
+                        if current_input > previous_input:
+                            print('Closer')
+                        else:
+                            print('Further')
+                    elif previous_input > number_to_guess and not is_user_guess:
+                        if current_input > previous_input:
+                            print('Further')
+                        else:
+                            print('Closer')
+            previous_input = current_input
+            is_first_iteration = False
+        except Exception as err:
+            with open(self.filepath, 'a+') as err_report:
+                err_report.write(str(err) + '\n')
+                err_report.close()
+            print(err)
+        except KeyboardInterrupt:
+            with open(self.filepath, 'a+') as err_report:
+                err_report.write('User ended script' + '\n')
+                err_report.close()
+            print('User ended script')
         return current_input
 
     def create_logs(self):
@@ -69,41 +96,36 @@ class Guess_Test:
                 user_values.close()
             number_to_guess = self.random_number()
             try_counter = 0
-            is_user_guess = False
-            is_first_iteration = True
-            previous_input = -1
             while not is_user_guess:
-                user_input = self.validation()
-                self.comparison(user_input, number_to_guess)
-                if user_input == number_to_guess:
+                try:
+                    user_input = self.validation()
+                    user_input = int(user_input)
+                    self.comparison(user_input, number_to_guess)
+                    if user_input == number_to_guess:
+                        try_counter += 1
+                        end_datetime = time.localtime()
+                        end_time = time.strftime('%H:%M:%S', end_datetime)
+                        with open(self.filepath, 'a+') as add_last_points:
+                            add_last_points.write('3) End time: ' + end_time + '\n')
+                            add_last_points.write('4) User tries: ' + str(try_counter) + '\n')
+                            add_last_points.close()
+                    else:
+                        is_valid_input = False
                     try_counter += 1
-                    end_datetime = time.localtime()
-                    end_time = time.strftime('%H:%M:%S', end_datetime)
-                    with open(self.filepath, 'a+') as add_last_points:
-                        add_last_points.write('3) End time: ' + end_time + '\n')
-                        add_last_points.write('4) User tries: ' + str(try_counter) + '\n')
-                        add_last_points.close()
-                    is_user_guess = True
-                if not is_first_iteration:
-                    if previous_input != user_input:
-                        if previous_input < number_to_guess and not is_user_guess:
-                            if user_input > previous_input:
-                                print('Closer')
-                            else:
-                                print('Further')
-                        elif previous_input > number_to_guess and not is_user_guess:
-                            if user_input > previous_input:
-                                print('Further')
-                            else:
-                                print('Closer')
-                previous_input = user_input
-                is_first_iteration = False
-                try_counter += 1
-        except Exception as err:
-            with open(self.filepath, 'a+') as bug_report:
-                bug_report.write(str(err))
-                bug_report.close()
+                except ValueError:
+                    try_counter += 1
+                    print('You entered not number or left empty field')
+        except BaseException as err:
+            with open(self.filepath, 'a+') as err_report:
+                err_report.write(str(err) + '\n')
+                err_report.close()
+        except KeyboardInterrupt:
+            with open(self.filepath, 'a+') as err_report:
+                err_report.write('User ended script' + '\n')
+                err_report.close()
 
 
 test = Guess_Test()
 test.function()
+
+
