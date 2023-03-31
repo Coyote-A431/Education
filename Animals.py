a5037ef65c69dbe51a6832d8dd624eb143b01dac
Unknown <as1qwe12@gmail.com>
2023-03-31 13:33:34 +0400

    Add all except 3rd point in main menu

--- Animals.py
+++ Animals.py
@@ -2,71 +2,6 @@ import os
 import re
 
 
-def get_path():
-    is_path_correct = False
-    while not is_path_correct:
-        try:
-            user_path = input('Введите путь до нужного файла: ')
-            num_files = len([f for f in os.listdir(user_path)
-                             if os.path.isfile(os.path.join(user_path, f))])
-            if num_files > 0:
-                is_path_correct = True
-            else:
-                print('В введённом Вами пути не существует файлов')
-        except FileNotFoundError:
-            print('Вы ввели несуществующий путь до файла')
-        except OSError:
-            print('Вы ввели несуществующий путь до файла')
-    return user_path
-
-current_path = get_path()
-
-def get_file_list(current_path):
-    animal_list = []
-    with os.scandir(current_path) as files:
-        for file in files:
-            animal_list.append(file.name)
-    print(animal_list)
-    return animal_list
-
-# get_file_list(current_path)
-
-def get_parsed_data(current_path):
-    # current_path = r'C:\Users\Unknown\Desktop\Education\Animal'
-    file_list = []
-    with os.scandir(current_path) as files:
-        for file in files:
-            file_list.append(file.name)
-    # files_locations = [f'{user_path}\\{animal}' for animal in animal_list]
-    files_locations = {animal: f'{current_path}\\{animal}' for animal in file_list}
-    # f = []
-    # for animal in animal_list:
-    #     f.append(f'{user_path}\\{animal}')
-    animal_data_dict = {}  # Словарь {имя файла: значения файла в виде словаря}
-    # file_content = ''
-    for file_name, path in files_locations.items():
-        with open(path, 'r', encoding='utf-8') as reader:
-            file_content = reader.read()
-            # for all_lines in reader:
-            #     if all_lines != '':
-            #         file_content += all_lines
-        animal_attributes_dict = {}  # Словарь с атрибутами животных {название атрибута: название значения}
-        attribute_row = file_content.split('\n')
-        if attribute_row[-1] == '':
-            attribute_row.pop(-1)
-        # attribute_row.remove('')
-        for i in attribute_row:
-            # attribute_value_list = i.split(':')
-            attribute_name, attribute_value = i.split(':')
-            attribute_name, attribute_value = attribute_name.strip(), attribute_value.strip()
-            # attribute_name = i.split(':')[0]
-            # attribute_value = i.split(':')[1]
-            animal_attributes_dict[attribute_name] = attribute_value
-        animal_data_dict[file_name] = animal_attributes_dict
-    return animal_data_dict
-
-p = get_parsed_data(current_path)
-
 class Animal:
 
     full_attribute_names_map = {'name': 'Наименование',
@@ -85,7 +20,16 @@ class Animal:
         inner_attributes_dict[index_for_inner_attributes] = attribute_name
         index_for_inner_attributes += 1
 
-    def __init__(self, name, genus, species, avg_lifetime, avg_height, avg_weight, place, country, file_name):
+    def __init__(self,
+                 name,
+                 genus,
+                 species,
+                 avg_lifetime,
+                 avg_height,
+                 avg_weight,
+                 place,
+                 country,
+                 ):
         self.__name = name
         self.__genus = genus
         self.__species = species
@@ -94,7 +38,6 @@ class Animal:
         self.__avg_weight = avg_weight
         self.__place = place
         self.__country = country
-        self.__file_name = file_name
 
     def __repr__(self):
         return self.__name
@@ -187,11 +130,70 @@ class Animal:
         else:
             print('Для данного пункта можно вводить только русские буквы и запятые')
 
-    def info(self):
-        attributes_dict = self.__dict__
-        attributes_dict.pop('_file_name')
-        for attribute_name, attribute_value in attributes_dict.items():
-            print(self.full_attribute_names_map[attribute_name] + ': ' + attribute_value)
+    def info(self, user_input_int):
+        for attribute_name, attribute_value in self.full_attribute_names_map.items():
+            a = getattr(animal_dict[user_input_int], attribute_name)
+            print(f'{attribute_value}: {a}')
+
+
+def get_path():
+    is_path_correct = False
+    while not is_path_correct:
+        try:
+            user_path = input('Введите путь до нужного файла: ')
+            num_files = len([f for f in os.listdir(user_path)
+                             if os.path.isfile(os.path.join(user_path, f))])
+            if num_files > 0:
+                is_path_correct = True
+            else:
+                print('В введённом Вами пути не существует файлов')
+        except FileNotFoundError:
+            print('Вы ввели несуществующий путь до файла')
+        except OSError:
+            print('Вы ввели несуществующий путь до файла')
+    return user_path
+
+def get_file_list(current_path):
+    animal_list = []
+    with os.scandir(current_path) as files:
+        for file in files:
+            animal_list.append(file.name)
+    # print(animal_list)
+    return animal_list
+
+def get_parsed_data(current_path):
+    # current_path = r'C:\Users\Unknown\Desktop\Education\Animal'
+    file_list = []
+    with os.scandir(current_path) as files:
+        for file in files:
+            file_list.append(file.name)
+    # files_locations = [f'{user_path}\\{animal}' for animal in animal_list]
+    files_locations = {animal: f'{current_path}\\{animal}' for animal in file_list}
+    # f = []
+    # for animal in animal_list:
+    #     f.append(f'{user_path}\\{animal}')
+    animal_data_dict = {}  # Словарь {имя файла: значения файла в виде словаря}
+    # file_content = ''
+    for file_name, path in files_locations.items():
+        with open(path, 'r', encoding='utf-8') as reader:
+            file_content = reader.read()
+            # for all_lines in reader:
+            #     if all_lines != '':
+            #         file_content += all_lines
+        animal_attributes_dict = {}  # Словарь с атрибутами животных {название атрибута: название значения}
+        attribute_row = file_content.split('\n')
+        if attribute_row[-1] == '':
+            attribute_row.pop(-1)
+        # attribute_row.remove('')
+        for i in attribute_row:
+            # attribute_value_list = i.split(':')
+            attribute_name, attribute_value = i.split(':')
+            attribute_name, attribute_value = attribute_name.strip(), attribute_value.strip()
+            # attribute_name = i.split(':')[0]
+            # attribute_value = i.split(':')[1]
+            animal_attributes_dict[attribute_name] = attribute_value
+        animal_data_dict[file_name] = animal_attributes_dict
+    return animal_data_dict
 
 def create_class_objects():
     parsed_animal_dict = get_parsed_data(current_path)
@@ -208,26 +210,18 @@ def create_class_objects():
                         avg_weight=animal_attributes['Средняя масса тела'],
                         place=animal_attributes['Место обитания'],
                         country=animal_attributes['Страна обитания'],
-                        file_name=file_name,
                         )
         # TODO добавить обработку исключения, если в файле будут не все атрибуты
         animal_file_name_dict[index] = file_name
         animal_dict[index] = animal
         index += 1
-    print(animal_file_name_dict)
     result.append(animal_dict)
     result.append(animal_file_name_dict)
     return result
 
-animal_info_data_list = create_class_objects()
-animal_dict = animal_info_data_list[0]
-animal_file_name_dict = animal_info_data_list[1]
-
 def print_animal_dict(animal_dict):
     for animal_name in animal_dict.values():
         print(f'{animal_name}')
-
-# print_animal_dict(animal_dict)
 
 def show_animal_data(animal_dict):
     is_correct_input = False
@@ -244,9 +238,7 @@ def show_animal_data(animal_dict):
                 raise ValueError
         except ValueError:
             print('Введён некорректный номер')
-    animal_dict[user_input_int].info()
-
-# show_animal_data(animal_dict)
+    animal_dict[user_input_int].info(user_input_int)
 
 def change_animal_data(animal_dict):
     is_correct_input_animal_num = False
@@ -273,7 +265,7 @@ def change_animal_data(animal_dict):
         user_input_attribute = input()
         try:
             user_input_attribute_int = int(user_input_attribute)
-            if user_input_attribute_int in range(1, index_for_outer_attributes + 1):
+            if user_input_attribute_int in range(1, index_for_outer_attributes):
                 is_corr_symb = False
                 while not is_corr_symb:
                     new_animal_value = input('Введите новое значение: ')
@@ -294,6 +286,7 @@ def change_animal_data(animal_dict):
                             is_correct_input_attribute_num = True
                         elif yes_or_no == 'n':
                             is_correct_yes_or_no = True
+                            is_corr_symb = True
                         else:
                             print(f'Введите "Да" (y) или "Нет" (n) согласно регистру')
             else:
@@ -302,20 +295,19 @@ def change_animal_data(animal_dict):
             print('Введён некорректный номер')
     return user_animal_input_int
 
-# b = change_animal_data(animal_dict, animal_file_name_dict)
-
 def save_and_exit(current_path, changed_animal_file_names_set, animal_file_name_dict):
     for index in animal_dict.keys():
         if index in changed_animal_file_names_set:
-            open(current_path + '\\' + animal_file_name_dict[index], 'w').close()
             attribute_prog_name_index = 1
+            new_full_string_info = ''
             for attribute_name in Animal.full_attribute_names_map.values():
-                new_attribute_value = getattr(animal_dict[index], Animal.inner_attributes_dict[attribute_prog_name_index])
-                with open(current_path + '\\' + animal_file_name_dict[index], 'a+', encoding='utf-8') as writer:
-                    writer.write(f'{attribute_name}: {new_attribute_value}\n')
+                new_attribute_value = getattr(animal_dict[index],
+                                              Animal.inner_attributes_dict[attribute_prog_name_index])
+                new_single_string_info = attribute_name + ': ' + new_attribute_value + '\n'
+                new_full_string_info += new_single_string_info
                 attribute_prog_name_index += 1
-
-# save_and_exit(current_path, changed_animal_file_names_list)
+            with open(current_path + '\\' + animal_file_name_dict[index], 'w', encoding='utf-8') as writer:
+                writer.write(f'{new_full_string_info}')
 
 def main_menu():
     is_correct_point = False
@@ -340,28 +332,30 @@ def main_menu():
         else:
             print('Введено некорректное значение')
 
+
+current_path = get_path()
+
+# get_file_list(current_path)
+
+p = get_parsed_data(current_path)
+
+animal_info_data_list = create_class_objects()
+animal_dict = animal_info_data_list[0]
+animal_file_name_dict = animal_info_data_list[1]
+
+# print_animal_dict(animal_dict)
+
+# show_animal_data(animal_dict)
+
+# b = change_animal_data(animal_dict, animal_file_name_dict)
+
+# save_and_exit(current_path, changed_animal_file_names_list)
+
 main_menu()
 
 
-# TODO setattr
 
-# for i in Animal.inner_attributes_dict.keys():
-#     print(Animal.inner_attributes_dict[i])
 
-# for index in animal_dict.keys():
-#     if index in range(1, 3):
-        # c = getattr(animal_dict[index], Animal.inner_attributes_dict[1])
-        # print(c)
-        # for j in changed_animal_file_names_set:
-            # c = getattr(animal_dict[j], Animal.inner_attributes_dict[att])
-        # for i in Animal.inner_attributes_dict.keys():
-            # for attribute_prog_name, attribute_name in Animal.full_attribute_names_map.items():
-            # c = getattr(animal_dict[index], Animal.inner_attributes_dict[i])
-            # print(c)
-                # correct_attribute_prog_name = '_Animal__' + attribute_prog_name
-                # c = getattr(animal_dict[j], Animal.inner_attributes_dict[attribute_prog_name])
-                # print(c)
-                # with open(current_path + '\\' + animal_file_name_dict[index], 'a+', encoding='utf-8') as writer:
-                #     writer.write(f'{attribute_name}: {animal_dict[index].__dict__[correct_attribute_prog_name]}\n')
+
 
 
